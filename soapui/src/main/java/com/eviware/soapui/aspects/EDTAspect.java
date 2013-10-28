74b99894b4829e29577b0d3f8af7c43e254fabb0
anders.jaensson <anders.jaensson@smartbear.com>
2013-10-28 08:39:41 +0100

    Improved output, only check com.eviware.*

--- soapui/src/main/java/com/eviware/soapui/aspects/EDTAspect.java
+++ soapui/src/main/java/com/eviware/soapui/aspects/EDTAspect.java
@@ -11,11 +11,13 @@ import java.awt.*;
 public class EDTAspect
 {
 
-	@Pointcut("call (* javax.swing..*+.*(..)) || " +
-			"call (javax.swing..*+.new(..))")
-	public void swingMethods() {}
+	@Pointcut( "call (* javax.swing..*+.*(..)) || " +
+			"call (javax.swing..*+.new(..))" )
+	public void swingMethods()
+	{
+	}
 
-	@Pointcut("call (* javax.swing..*+.add*Listener(..)) || "
+	@Pointcut( "call (* javax.swing..*+.add*Listener(..)) || "
 			+ "call (* javax.swing..*+.remove*Listener(..)) || "
 			+ "call (* javax.swing..*+.getListeners(..)) || "
 			+ "call (* javax.swing..*+.revalidate()) || "
@@ -25,14 +27,22 @@ public class EDTAspect
 			+ "target (javax.swing.SwingWorker+) || "
 			+ "call (* javax.swing.SwingUtilities+.invoke*(..)) || "
 			+ "call (* javax.swing.SwingUtilities+.isEventDispatchThread()) || "
-			+ "call (void javax.swing.JComponent+.setText(java.lang.String))")
-	public void safeMethods() {}
+			+ "call (void javax.swing.JComponent+.setText(java.lang.String))" )
+	public void safeMethods()
+	{
+	}
 
-	@Before("swingMethods() && !safeMethods() && !within(EDTAspect)")
-	public void checkCallingThread(JoinPoint.StaticPart thisJoinPointStatic) {
-		if(!EventQueue.isDispatchThread()) {
-			System.err.println("Swing EDT violation: " + thisJoinPointStatic.getSignature()
-					+ " (" + thisJoinPointStatic.getSourceLocation() + ")");
+	@Before( "swingMethods() && !safeMethods() && !within(EDTAspect)" )
+	public void checkCallingThread( JoinPoint.StaticPart thisJoinPointStatic )
+	{
+		if( !EventQueue.isDispatchThread() )
+		{
+			Thread currentThread = Thread.currentThread();
+			StringBuilder sb = new StringBuilder();
+			sb.append(String.format("Swing EDT violation in thread '%s (%d)': \n", currentThread.getName(), currentThread.getId()));
+			sb.append( thisJoinPointStatic.getSignature() );
+			sb.append(" (").append( thisJoinPointStatic.getSourceLocation() ).append( ")" );
+			System.err.println( sb.toString() );
 			Thread.dumpStack();
 		}
 	}
